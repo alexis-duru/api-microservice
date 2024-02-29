1e2ec33a9dc662aa3227ae0399b6117a0febc46e
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-29 14:25:49 +0100

    fix

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 const express = require("express");
-// import { router as orderRouter } from "./routes/order.route.js";
-// import bodyParser from "body-parser";
+// const bodyParser = require("body-parser");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
 app.get("/api/order", (req, res) => {
   res.send("PONG");
 });
-
 // app.use("/api", orderRouter);
 
 app.listen(3000, () => {
   console.log("Running on port 3000.");
 });
 
-module.exports = app;
+// Create a route app.post('/api/order', (req, res) => { res.send('Order created!'); });
+
+app.post("/api/order", (req, res) => {
+  // const testProduct = {
+  //   nom: "Un produit random",
+  //   prix: 10.99,
+  //   quantite: 1,
+  // };
+  // if (!availableProduct(testProduct)) {
+  //   return res.status(400).json({
+  //     message: "Le produit n'est pas disponible à la commande",
+  //   });
+  // }
+  // req.body.testProduct = testProduct;
+  // res.status(201).json({
+  //   message: `create one order`,
+  //   order: {
+  //     id: createRandomId(),
+  //   },
+  // });
+  // function availableProduct(produit) {
+  //   if (produit.quantite > 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // function createRandomId() {
+  //   return Math.floor(Math.random() * 1000);
+  // }
+});
