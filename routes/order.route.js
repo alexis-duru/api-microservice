afce6f82ea99a35687da77860ae11214975ecfd4
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-29 13:50:34 +0100

    fix

--- routes/order.route.js
+++
@@ -1,6 +0,0 @@
-// import express from "express";
-// import { createOneOrder } from "../controllers/order.controller.js";
-
-// export const router = express.Router();
-
-// router.route("/order").post(createOneOrder);
