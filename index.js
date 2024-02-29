43da59c74234cdc012560c3ee1cffe58f894c3b2
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-29 13:43:13 +0100

    fix

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-// import express from "express";
 // Add Express
 const express = require("express");
 
 // Initialize Express
-// import { router as orderRouter } from "./routes/order.route.js";
-// import bodyParser from "body-parser";
-
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
+// Create GET request
 app.get("/api/order", (req, res) => {
   res.send("PONG");
 });
 
-// app.use("/api", orderRouter);
-
+// Initialize server
 app.listen(3000, () => {
   console.log("Running on port 3000.");
 });
