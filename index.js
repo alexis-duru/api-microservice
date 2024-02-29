9fab99935eddf630ab95c61d7fb8cfef9bf8f0d5
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-29 16:09:36 +0100

    create order

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios").default;
 
 const app = express();
 
@@ -9,45 +10,113 @@ app.use(bodyParser.json());
 app.get("/api/order", (req, res) => {
   res.send("PONG");
 });
-// app.use("/api", orderRouter);
 
 app.listen(3000, () => {
   console.log("Running on port 3000.");
 });
 
-app.post("/api/order", (req, res) => {
-  const testProduct = {
-    nom: "Un produit random",
-    prix: 10.99,
-    quantite: 1,
-  };
+app.post("/api/order", async (req, res) => {
+  const status = "pending";
 
-  if (!availableProduct(testProduct)) {
+  productArray = req.body.products;
+
+  if (!availableProduct(productArray)) {
     return res.status(400).json({
       message: "Le produit n'est pas disponible à la commande",
     });
   }
 
-  req.body.testProduct = testProduct;
+  for (const product of productArray) {
+    const url = `https://api-stock.vercel.app/api/stock/${product.id}/movement`;
+    try {
+      await axios.post("/user", {
+        productId: product.id,
+        quantity: product.quantity,
+        status: "Reserve",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   res.status(201).json({
-    message: `create one order`,
-    order: {
-      id: createRandomId(),
-    },
+    id: createRandomId(),
   });
 
-  function availableProduct(produit) {
-    if (produit.quantite > 0) {
-      return true;
-    } else {
-      return false;
-    }
+  function availableProduct(productArray) {
+    return productArray.some((product) => product.quantite > 0);
   }
 });
 
 function createRandomId() {
   return Math.floor(Math.random() * 1000);
 }
+
+// app.put("/api/order/:id", (req, res) => {
+//   const orderId = req.params.id;
+//   const status = req.body.status;
+
+//   if (status === "pending") {
+//     return res.status(400).json({
+//       message: "La commande est en attente de validation",
+//     });
+//   }
+
+//   if (status === "delivered") {
+//     return res.status(400).json({
+//       message: "La commande a été livrée",
+//     });
+//   }
+
+//   res.status(200).json({
+//     message: "La commande a été modifiée avec succès",
+//   });
+// });
+
+// app.post("/api/shipping", (req, res) => {
+//   const orderId = req.body.orderId;
+//   const productArray = req.body.productArray;
+
+//   if (orderId === 0) {
+//     return res.status(404).json({
+//       message: "La commande n'existe pas",
+//     });
+//   }
+
+//   const totalQuantity = productArray.reduce(
+//     (acc, product) => acc + product.quantite,
+//     0
+//   );
+
+//   res.status(200).json({
+//     orderId: orderId,
+//     totalQuantity: totalQuantity,
+//   });
+// });
+
+// app.get("/api/order/:id", (req, res) => {
+//   const orderId = req.params.id;
+
+//   if (orderId === "0") {
+//     return res.status(404).json({
+//       message: "La commande n'existe pas",
+//     });
+//   }
+
+//   res.status(200).json({
+//     orderId: orderId,
+//     status: "pending",
+//     productArray: [
+//       {
+//         productId: 1,
+//         quantite: 1,
+//       },
+//       {
+//         productId: 2,
+//         quantite: 0,
+//       },
+//     ],
+//   });
+// });
 
 module.exports = app;
