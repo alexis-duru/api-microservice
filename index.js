dc3245e811bf19f864470931309d860dbeca5c57
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-29 14:38:28 +0100

    add create product

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/api/order", (req, res) => {
   res.send("PONG");
@@ -16,39 +16,38 @@ app.listen(3000, () => {
 });
 
 app.post("/api/order", (req, res) => {
-  res.send("Order created!");
+  const testProduct = {
+    nom: "Un produit random",
+    prix: 10.99,
+    quantite: 1,
+  };
+
+  if (!availableProduct(testProduct)) {
+    return res.status(400).json({
+      message: "Le produit n'est pas disponible à la commande",
+    });
+  }
+
+  req.body.testProduct = testProduct;
+
+  res.status(201).json({
+    message: `create one order`,
+    order: {
+      id: createRandomId(),
+    },
+  });
+
+  function availableProduct(produit) {
+    if (produit.quantite > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
 
-// Create a route app.post('/api/order', (req, res) => { res.send('Order created!'); });
-
-// app.post("/api/order", (req, res) => {
-// const testProduct = {
-//   nom: "Un produit random",
-//   prix: 10.99,
-//   quantite: 1,
-// };
-// if (!availableProduct(testProduct)) {
-//   return res.status(400).json({
-//     message: "Le produit n'est pas disponible à la commande",
-//   });
-// }
-// req.body.testProduct = testProduct;
-// res.status(201).json({
-//   message: `create one order`,
-//   order: {
-//     id: createRandomId(),
-//   },
-// });
-// function availableProduct(produit) {
-//   if (produit.quantite > 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// function createRandomId() {
-//   return Math.floor(Math.random() * 1000);
-// }
-// });
+function createRandomId() {
+  return Math.floor(Math.random() * 1000);
+}
 
 module.exports = app;
