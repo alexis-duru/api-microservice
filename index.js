d93394215886545c20ba1c358701f4d43086ccc8
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-03-01 10:28:24 +0100

    add product

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.listen(3000, () => {
 });
 
 app.post("/api/order", async (req, res) => {
-  productArray = req.body.products;
+  allProducts = req.body.products;
 
-  for (const product of productArray) {
-    const reservationProduct = `https://api-stock.vercel.app/api/stock/${product.id}/movement`;
+  for (const product of allProducts) {
+    const reservationProduct = `https://api-stock.vercel.app/api/stock/${product.productId}/movement`;
     try {
       await axios.post(reservationProduct, {
-        productId: product.id,
+        productId: product.productId,
         quantity: product.quantity,
         status: "Reserve",
       });
@@ -33,18 +33,24 @@ app.post("/api/order", async (req, res) => {
     }
   }
 
-  // Réponse au client
+  const orderId = createRandomId();
+
   res.status(201).json({
-    id: createRandomId(),
+    id: orderId,
   });
 
-  orders[createRandomId()] = productArray;
+  orders[orderId] = allProducts;
+
+  const nbProducts = allProducts.reduce(
+    (acc, product) => acc + product.quantity,
+    0
+  );
 
   const NotificationShippingUrl = `http://microservices.tp.rjqu8633.odns.fr/api/shipping/`;
   try {
     await axios.post(NotificationShippingUrl, {
-      orderId: createRandomId(),
-      productArray: productArray,
+      orderId,
+      nbProducts,
     });
   } catch (error) {
     console.log(error);
@@ -57,39 +63,39 @@ function createRandomId() {
 
 // Get de la commande
 
-app.get("/api/order/:id", async (req, res) => {
-  const orderId = req.params.id;
+// app.get("/api/order/:id", async (req, res) => {
+//   const orderId = req.params.id;
 
-  const url = `http://microservices.tp.rjqu8633.odns.fr/api/order/${orderId}`;
+//   const url = `http://microservices.tp.rjqu8633.odns.fr/api/order/${orderId}`;
 
-  try {
-    const response = await axios.get(url);
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+//   try {
+//     const response = await axios.get(url);
+//     res.status(200).json(response.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // Changement du statut de la commande
 
-app.put("/api/order/:id", async (req, res) => {
-  const orderId = req.params.id;
-  const status = req.body.status;
+// app.put("/api/order/:id", async (req, res) => {
+//   const orderId = req.params.id;
+//   const status = req.body.status;
 
-  const url = `http://microservices.tp.rjqu8633.odns.fr/api/order/${orderId}`;
+//   const url = `http://microservices.tp.rjqu8633.odns.fr/api/order/${orderId}`;
 
-  try {
-    await axios.put(url, {
-      status: status,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+//   try {
+//     await axios.put(url, {
+//       status: status,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
 
-  res.status(200).json({
-    message: "La commande a été modifiée avec succès",
-  });
-});
+//   res.status(200).json({
+//     message: "La commande a été modifiée avec succès",
+//   });
+// });
 
 // app.post("/api/shipping", async (req, res) => {
 //   const orderId = req.body.orderId;
